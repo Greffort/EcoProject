10bf42942c03542e13678e9f0a72cdf6710b7ccf
Ritboy <ritboy@mail.ru>
2017-04-22 04:31:08 +0400

    Добавил исходную комбинацию точек в виде примера

--- EcoProject/EcoProject/DrawPanelForm.cs
+++ EcoProject/EcoProject/DrawPanelForm.cs
@@ -34,7 +34,6 @@ namespace EcoProject
             sheet.Image = draw.GetBitmap();
 
             selectButton.Visible = false;
-            drawEdgeButton.Visible = false;
             deleteALLButton.Visible = false;
             deleteButton.Visible = false;
         }
@@ -44,7 +43,6 @@ namespace EcoProject
         {
             selectButton.Enabled = false;
             drawVertexButton.Enabled = true;
-            drawEdgeButton.Enabled = true;
             deleteButton.Enabled = true;
             draw.clearSheet();
             draw.drawALLGraph(graph);
@@ -57,7 +55,6 @@ namespace EcoProject
         {
             drawVertexButton.Enabled = false;
             selectButton.Enabled = true;
-            drawEdgeButton.Enabled = true;
             deleteButton.Enabled = true;
             draw.clearSheet();
             //draw.drawALLGraph(graph);
@@ -70,7 +67,6 @@ namespace EcoProject
             deleteButton.Enabled = false;
             selectButton.Enabled = true;
             drawVertexButton.Enabled = true;
-            drawEdgeButton.Enabled = true;
             draw.clearSheet();
             draw.drawALLGraph(graph);
             sheet.Image = draw.GetBitmap();
@@ -81,7 +77,6 @@ namespace EcoProject
         {
             selectButton.Enabled = true;
             drawVertexButton.Enabled = true;
-            drawEdgeButton.Enabled = true;
             deleteButton.Enabled = true;
             const string message = "Вы действительно хотите полностью удалить граф?";
             const string caption = "Удаление";
@@ -147,11 +142,7 @@ namespace EcoProject
                 Triangulator triangulate = new Triangulator();
                 List<Vertex> listvertex = new List<Vertex>();
 
-                SetVectors();
-                foreach (Vertex vert in V)
-                {
-                    draw.drawVector(vert);
-                }
+                //SetVectors();
                 List<Triangle> tr = triangulate.Triangulation(V);
                 Monitoring monitor = new Monitoring(tr);
                 monitor.GetArrayOfOutsideEdge();
@@ -159,24 +150,25 @@ namespace EcoProject
                 foreach (Triangle t in monitor.triangles)
                 {
                     draw.BrushTriangle(t);
-                    richTextBox1.Text += t.ToString();
-                }
-
-                foreach (Triangle t in tr)
-                {
                     draw.drawTriangle(t);
-                }
-
-                //переписать не под вызов конструктора, а под вызов метода, который будет сравнивать треугольники.
-                //т.е. Equals для треугольников, который сравнивает по вершинам.
-
-                foreach (Triangle t in tr)
-                {
                     for (int i = 0; i < t.edgemas.Length; i++)
                     {
                         if (t.edgemas[i].isOutside) draw.drawEdge(t.edgemas[i]);
                         //if (t.edgemas[i].brother != null) G.drawEdge(t.edgemas[i].brother, new Pen(Color.Aqua));
                     }
+                    richTextBox1.Text += t.ToString();
+                }
+                for (int i = 0; i < V.Count; i++)
+                {
+                    draw.drawVertex(V[i], (i + 1).ToString());
+                }
+
+                //переписать не под вызов конструктора, а под вызов метода, который будет сравнивать треугольники.
+                //т.е. Equals для треугольников, который сравнивает по вершинам.
+
+                foreach (Vertex vert in V)
+                {
+                    draw.drawVector(vert);
                 }
                 sheet.Image = draw.GetBitmap();
             }
@@ -209,13 +201,50 @@ namespace EcoProject
                     flagempty = false;
                 }
             }
-
+            flagempty = true; // удалить эту строчку
             return flagempty;
         }
 
         private void detail_btn_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void example_button_Click(object sender, EventArgs e)
+        {
+            V.Clear();
+            E.Clear();
+            // умножаем все значения на 20
+            float multipl = 20f;
+            V.Add(new Vertex(2 * multipl, 8 * multipl));
+            V[0].Vector = new Vector(0 * multipl, 0 * multipl);
+            V.Add(new Vertex(100, 300));
+            V[1].Vector = new Vector(1 * multipl, -1 * multipl);
+            V.Add(new Vertex(120, 40));
+            V[2].Vector = new Vector(1 * multipl, 1 * multipl);
+            V.Add(new Vertex(160, 200));
+            V[3].Vector = new Vector(2 * multipl, 1 * multipl);
+            V.Add(new Vertex(220, 320));
+            V[4].Vector = new Vector(3 * multipl, 0 * multipl);
+            V.Add(new Vertex(240, 100));
+            V[5].Vector = new Vector(0 * multipl, 0 * multipl);
+            V.Add(new Vertex(260, 180));
+            V[6].Vector = new Vector(2 * multipl, 1 * multipl);
+            V.Add(new Vertex(17 * multipl, 1 * multipl));
+            V[7].Vector = new Vector(1 * multipl, 0 * multipl);
+            V.Add(new Vertex(17 * multipl, 7 * multipl));
+            V[8].Vector = new Vector(1 * multipl, 2 * multipl);
+            V.Add(new Vertex(18 * multipl, 13 * multipl));
+            V[9].Vector = new Vector(3 * multipl, 1 * multipl);
+            V.Add(new Vertex(20 * multipl, 9 * multipl));
+            V[10].Vector = new Vector(2 * multipl, 2 * multipl);
+            for (int i = 0; i < V.Count; i++)
+            {
+                draw.drawVertex(V[i], (i + 1).ToString());
+                draw.drawVector(V[i]);
+                dataGridView1.Rows.Add(i + 1, V[i], V[i].Vector.x, V[i].Vector.y);
+            }
+            sheet.Image = draw.GetBitmap();
         }
     }
 }
--- EcoProject/EcoProject/DrawPanelForm.designer.cs
+++ EcoProject/EcoProject/DrawPanelForm.designer.cs
@@ -31,7 +31,6 @@
             this.selectButton = new System.Windows.Forms.Button();
             this.deleteALLButton = new System.Windows.Forms.Button();
             this.deleteButton = new System.Windows.Forms.Button();
-            this.drawEdgeButton = new System.Windows.Forms.Button();
             this.drawVertexButton = new System.Windows.Forms.Button();
             this.sheet = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -42,6 +41,8 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.AddVertex_button = new System.Windows.Forms.Button();
+            this.example_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -164,12 +165,31 @@
             this.richTextBox1.TabIndex = 18;
             this.richTextBox1.Text = "";
             // 
+            // AddVertex_button
+            // 
+            this.AddVertex_button.Location = new System.Drawing.Point(145, 456);
+            this.AddVertex_button.Name = "AddVertex_button";
+            this.AddVertex_button.Size = new System.Drawing.Size(98, 23);
+            this.AddVertex_button.TabIndex = 20;
+            this.AddVertex_button.Text = "Добавить точку";
+            // 
+            // example_button
+            // 
+            this.example_button.Location = new System.Drawing.Point(249, 456);
+            this.example_button.Name = "example_button";
+            this.example_button.Size = new System.Drawing.Size(98, 23);
+            this.example_button.TabIndex = 21;
+            this.example_button.Text = "Пример";
+            this.example_button.Click += new System.EventHandler(this.example_button_Click);
+            // 
             // DrawPanelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1155, 491);
+            this.Controls.Add(this.example_button);
+            this.Controls.Add(this.AddVertex_button);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.detail_btn);
@@ -177,12 +197,10 @@
             this.Controls.Add(this.selectButton);
             this.Controls.Add(this.deleteALLButton);
             this.Controls.Add(this.deleteButton);
-            this.Controls.Add(this.drawEdgeButton);
             this.Controls.Add(this.drawVertexButton);
             this.Controls.Add(this.sheet);
             this.Name = "DrawPanelForm";
             this.Text = "Eco";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.sheet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -193,7 +211,6 @@
 
         private System.Windows.Forms.PictureBox sheet;
         private System.Windows.Forms.Button drawVertexButton;
-        private System.Windows.Forms.Button drawEdgeButton;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.Button deleteALLButton;
         private System.Windows.Forms.Button selectButton;
@@ -206,6 +223,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button AddVertex_button;
+        private System.Windows.Forms.Button example_button;
     }
 }
 
